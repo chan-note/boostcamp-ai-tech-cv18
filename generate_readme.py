3b47cf1a28c11859ab9b367d2bb14a9be1402b90
nOctaveLay <wjddkdml1124@naver.com>
2024-09-06 14:49:37 +0900

    update generate_readme

--- generate_readme.py
+++ generate_readme.py
@@ -1,4 +1,9 @@
 from datetime import datetime, timedelta
+now = datetime.now()
+end = now + timedelta(days = 5)
+new_contents = ''
+
+date_format = f"### {now.strftime('%y.%m.%d')} {now.strftime('%a').upper()} - {end.strftime('%y.%m.%d')} {end.strftime('%a').upper()}\n\n"
 
 def find_topic(topic:str, content_list:list):
     i = 0
@@ -85,6 +90,59 @@ def add_paper(change_contents:str, text_file_path = "../README.md"):
                 new_contents += result_string + '\n'
             i += 1
     print(new_contents)
+
+def return_index_next_matching(matching:str, lines:list):
+    '''
+    lines에서 matching과 일치하는 곳이 있다면,
+    내가 이전에 추가한 내용인지 아닌지를 확인한다. (날짜가 같으면 보통 내가 추가한 것이다.)
+    그 곳의 index를 출력한다. (이는 줄 번호가 된다.)
+    일치하는 곳이 없다면 그 파일의 마지막을 출력한다.
+    '''
+    for line_number, line in enumerate(lines):
+        line = line.strip()
+        if matching == line[:len(matching)]:
+            # 날짜가 겹치는 지 확인한다.
+            # 겹치면 -1 (추가하지 말라)를 추가한다.
+            # 안 겹치면 그 줄의 line_number를 출력한다.
+                if matching == f"### {now.month}월 논문 발표" or matching == date_format:
+                    return -1
+                else:
+                   return line_number
+    line_number += 1 
+    return line_number
+
+def update_contents(contents:str, file_name:str, matching:str):
+    # README.md를 읽는다.
+    new_contents = ''
+    with open(file_name, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line_number, line_content in enumerate(lines):
+            new_contents += line_content
+            line_content = line_content.strip() # 줄 개행 문자 제거
+
+            # 만약, update를 원하는 곳을 찾았다면
+            if line_content == matching:
+                match_line_number = return_index_next_matching("## ", lines[line_number+1:])
+                
+                # 만약 contents가 겹친다면
+                if match_line_number == -1:
+                    # 뒤의 모든 내용을 새롭게 저장하고 break
+                    new_contents = "".join(lines[line_number+1:])
+                    return new_contents
+                
+                # contents가 겹치지 않는다면
+                else:
+                    new_contents += "".join(lines[line_number+1:line_number+1+match_line_number])
+                    # 그 matching된 곳에 써주고, 개행 문자를 남겨
+                    new_contents += contents
+                    new_contents += "".join(lines[line_number+1+match_line_number:])
+
+
+    # 해당하는 곳을 찾는다.
+    # 해당하는 곳에서 추가할 곳을 찾는다.
+    # 내가 추가할 날짜와 겹친다면 내용을 수정하지 않는다.
+    # 추가할 곳을 찾았다면, 바꾼다.
+    # 변경된 data로 README.md를 update한다.
 
 
 def update_week():
