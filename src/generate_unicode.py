d6871dca029d4df9824b2bf57b49e6794de232e4
nOctaveLay <wjddkdml1124@naver.com>
2024-09-09 18:04:22 +0900

    필요 없는 파일 제거

--- src/generate_unicode.py
+++
@@ -1,9 +0,0 @@
-escaped_str = input("이스케이프된 문자열을 입력하세요: ")
-
-# 문자열을 바이트로 변환
-byte_str = bytes(escaped_str.encode('utf-8').decode('unicode_escape'), 'latin1')
-
-# 바이트를 UTF-8로 디코딩하여 한국어로 변환
-decoded_str = byte_str.decode('utf-8')
-
-print("디코딩된 문자열:", decoded_str)
